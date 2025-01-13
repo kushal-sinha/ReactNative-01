88f842af722375a2a742f1b760f9531091d2c351
kushal-sinha <kushal147258369@gmail.com>
2025-01-13 15:30:34 +0530

    handling text input

---
+++ rn-starter/src/screens/TextScreen.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Text, TextInput, StyleSheet, View } from "react-native";
+
+const TextScreen = () => {
+    const [name, SetName] = useState('');
+    return (
+        <View>
+            <TextInput onChangeText={newValue =>
+                SetName(newValue)
+            } value={name} style={styles.input} autoCapitalize="none" autoCorrect={false} />
+
+            <Text>My name is {name}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+});
+export default TextScreen;
+
